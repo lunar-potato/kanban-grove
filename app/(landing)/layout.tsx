835c9ed4d782d8bc13968a5b82bb1ae64168a681
Gwyneth <118124645+lunar-potato@users.noreply.github.com>
2024-07-23 13:06:02 +0100

    Added footer will still need future changes

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "./_components/footer";
 import { Navbar } from "./_components/navbar";
 
 const LandingLayout = ({
@@ -11,7 +12,7 @@ const LandingLayout = ({
             <main className="pt-40 pb-20 bg-slate-100">
                 {children}
             </main>
-            {/* Footer */}
+            <Footer/>
         </div>
     );
 };
