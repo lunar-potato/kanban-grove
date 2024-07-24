36a075edb2969e52ef6346f979fa0021d458afad
Gwyneth <118124645+lunar-potato@users.noreply.github.com>
2024-07-24 17:23:07 +0100

    Changed minor styling

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -23,7 +23,7 @@ const LandingPage = () => {
     return (
         <div className="flex items-center justify-center flex-col">
             <div className="flex items-center justify-center flex-col">
-                <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-400 text-amber-700 rounded-full uppercase">
+                <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
                     <Kanban className="h-6 w-6 mr-2" />
                     Kanban Task Management
                 </div>
