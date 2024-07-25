d6d1b491f3c3831ad788e3e022603eecab6d7b44
Gwyneth <118124645+lunar-potato@users.noreply.github.com>
2024-07-25 21:23:17 +0100

    Added navbar to organization dashboard page if that makes sense

--- app/(landing)/_components/navbar.tsx
+++ app/(landing)/_components/navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export const Navbar = () => {
     return (
-        <div className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center">
+        <nav className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center">
             <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
                 <Logo />
                 <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
@@ -20,7 +20,7 @@ export const Navbar = () => {
                     </Button>
                 </div>
             </div>
-        </div>
+        </nav>
     );
 };
 
