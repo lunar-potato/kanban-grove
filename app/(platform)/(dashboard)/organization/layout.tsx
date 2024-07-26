23ac8711e8dba186eacc21244fb58894a4e5051f
Gwyneth <118124645+lunar-potato@users.noreply.github.com>
2024-07-26 19:31:52 +0100

    Added logic for sidebar and added more dependencies for sidebar

--- app/(platform)/(dashboard)/organization/layout.tsx
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -1,3 +1,5 @@
+import { Sidebar } from "../_components/sidebar";
+
 const organizationLayout = ({
     children
 }: {
@@ -7,7 +9,7 @@ const organizationLayout = ({
         <main className="pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-2-screen-xl mx-auto">
             <div className="flex gap-x-7">
                 <div className="w-64 shrink-0 hidden md:block">
-                    {/* Sidebar */}
+                    <Sidebar/>
                 </div>
             {children}
             </div>
