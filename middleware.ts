71d9b3fcc4f30697a61e93400ad81e55d5be5111
Gwyneth <118124645+lunar-potato@users.noreply.github.com>
2024-07-26 18:29:53 +0100

    Fixed middleware?

--- middleware.ts
+++ middleware.ts
@@ -1,41 +1,34 @@
-import { RedirectToSignIn } from "@clerk/nextjs";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const publicRoute = createRouteMatcher([
-  '/'
-]);
+const isPublicRoute = createRouteMatcher(['/']);
 
 export default clerkMiddleware((auth, req) => {
-  // If user is authenticated and on a public route
-  if (auth().userId && publicRoute(req)) {
-    let path = "/select-org";
-
-    if (auth().orgId) {
-      path = `/organization/${auth().orgId}`;
-    }
-
-    const orgSelection = new URL(path, req.url);
-    return NextResponse.redirect(orgSelection);
-  }
+  const { userId, orgId } = auth();
 
   // If user is not authenticated and not on a public route 
-  if (!auth().userId && !publicRoute(req)) {
-    return RedirectToSignIn({ redirectUrl: req.url })
+  if (!userId && !isPublicRoute(req)) {
+    return NextResponse.redirect(new URL('/sign-in', req.url));
   }
 
-  // If user is authenticated but does not have an organization and tring to access select-org
-  if (auth().userId && !auth().orgId && req.nextUrl.pathname !== "/select-org") {
-    const orgSelection = new URL("/select-org", req.url);
-    return NextResponse.redirect(orgSelection);
+  // If user is authenticated but does not have an organization
+  if (userId && !orgId && req.nextUrl.pathname !== "/select-org") {
+    return NextResponse.redirect(new URL("/select-org", req.url));
   }
+
+  // If user is authenticated and on a public route
+  if (userId && isPublicRoute(req)) {
+    const path = orgId ? `/organization/${orgId}` : "/select-org";
+    return NextResponse.redirect(new URL(path, req.url));
+  }
+
+  // Allow access for all other cases
+  return NextResponse.next();
 });
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
