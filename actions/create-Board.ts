22b63bee080f5867b0768f80ee889b9b5e93cbfd
Gwyneth <118124645+lunar-potato@users.noreply.github.com>
2024-09-10 16:59:04 +0100

    Error with state, form needs fixing

--- actions/create-Board.ts
+++ actions/create-Board.ts
@@ -3,21 +3,47 @@
 import { z } from "zod";
 import { db } from "@/lib/db";
 import { revalidatePath } from "next/cache";
+import { permanentRedirect } from "next/navigation";
+
+export type State = {
+  errors?: {
+    title?: string[];
+  };
+  message?: string | null;
+};
 
 const createBoard = z.object({
-    title: z.string(),
+  title: z.string().min(3, {
+    message: "Minimum of three (3) characters",
+  }),
 });
 
-export async function create(formData: FormData) {
- const { title } = createBoard.parse({
+export async function create(prevState: State, formData: FormData) {
+  const validatedFields = createBoard.safeParse({
     title: formData.get("title"),
- });
+  });
 
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields.",
+    };
+  }
+
+  const { title } = validatedFields.data;
+
+  try {
     await db.board.create({
-        data: {
-            title,
-        }
+      data: {
+        title,
+      },
     });
+  } catch (error) {
+    return {
+      message: "Database Error",
+    };
+  }
 
-    revalidatePath("/organization/org_2kFr6zKrZzEXec1B3ljCn6iIpOf")
+  revalidatePath("/organization/org_2kFr6zKrZzEXec1B3ljCn6iIpOf");
+  permanentRedirect("/organization/org_2kFr6zKrZzEXec1B3ljCn6iIpOf");
 }
--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -2,10 +2,15 @@
 
 import { create } from "@/actions/create-Board";
 import { Button } from "@/components/ui/button";
+import { useFormState } from "react-dom";
 
 export const Form = () => {
+    const initialState = { message: null, errors: {} };
+    const [state, dispatch] = useFormState(create, initialState);
+
     return (
-        <form action={create}>
+        <form action={dispatch}>
+            <div className="flex flex-col space-y-2">
             <input
                 id="title"
                 name="title"
@@ -13,6 +18,16 @@ export const Form = () => {
                 placeholder="Enter a board title"
                 className="border-black border p-1"
             />
+            {state?.errors?.title ? (
+                <div>
+                    {state.errors.title.map((error: string) => (
+                        <p key={error} className="text-rose-500">
+
+                        </p>
+                    ))}
+                </div>
+            ) : null}
+            </div>
             <Button type="submit">
                 Submit
             </Button>
